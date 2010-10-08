fb486084a3daed301423c5e6d3d0966ed4b77cec
Yi-Shin Li <ysli@araisrobo.com>
2010-10-08 17:16:49 +0800

    for MAILBOX: update RX_BURST_MIN for wou_recv()

--- src/wou/board.h
+++ src/wou/board.h
@@ -84,9 +84,10 @@ static const _Ftstat Ftstat[] = {
 #define TX_BURST_MAX    512
 #define TX_CHUNK_SIZE   512
 // to prevent from pending because of too large RX_CHUNK_SIZE: 
-// #define RX_CHUNK_SIZE   512
+//will_kill_mailbox: #define RX_CHUNK_SIZE   512
+//will_kill_mailbox: #define RX_BURST_MIN    256
 #define RX_CHUNK_SIZE   2048
-#define RX_BURST_MIN    256
+#define RX_BURST_MIN    32
 // #define RX_CHUNK_SIZE   4096
 // #define RX_BURST_MIN    512
 //failed@1.2KV,16ms: #define BURST_LIMIT   32 // for debugging
