2feef6c56ac35803421bdad9cb835718e409a20c
Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>
2011-03-01 11:53:30 +0800

    new field for index homing
    Signed-off-by: Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>

--- src/sync_cmd.h
+++ src/sync_cmd.h
@@ -111,6 +111,8 @@ enum motion_parameter_addr {
     MOTION_TYPE       ,
     HOME_SW_INPUT_ID  ,
     HOME_SW_ACTIVE    ,
+    INDEX_INPUT_ID,
+    INDEX_INTPUT_ACTIVE,
     // section for PID
         // unit: 1/2^20
     P_GAIN            ,
