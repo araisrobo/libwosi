51cdf9919e44c5e51d35438993dfec03b1de8728
Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>
2011-04-26 19:30:57 +0800

    remove unused field (CMD_FRACT_BIT)
    Signed-off-by: Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>

--- src/sync_cmd.h
+++ src/sync_cmd.h
@@ -102,13 +102,14 @@
 
 // memory map for motion parameter for each joint
 enum motion_parameter_addr {
-    CMD_FRACT_BIT     ,
+//    CMD_FRACT_BIT     ,
     PARAM_FRACT_BIT   ,
     MAX_VELOCITY      ,
     MAX_ACCEL         ,
     MAX_ACCEL_RECIP   ,
     COMP_VEL          ,
     MOTION_TYPE       ,
+    PREV_MOTION_TYPE  ,
     HOME_SW_INPUT_ID  ,
     HOME_SW_ACTIVE    ,
     INDEX_ENABLE      ,
